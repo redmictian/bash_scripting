d596d70dd71964a39239f20b0364b5d20f490bed
redmictian <redmictian@gmail.com>
2022-01-07 02:46:52 +0300

    Some grammar fixes

--- dir_script.sh
+++ dir_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo 'Enter the names of folder'
 read name
-[[ ! -d $name ]] && mkdir $name || { echo "are you that dumb nig?" && exit; }
+[[ ! -d $name ]] && mkdir $name || { echo "Dont do that" && exit; }
 for (( c=0; c<=3; c++ ))
 do  
    touch $name/$name.$c
-   echo "killaz is" > $name/$name.$c
+   echo "Writing to the files" > $name/$name.$c
    cat $name/$name.$c
 done
 ls $name
