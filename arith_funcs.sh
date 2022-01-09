d95c1fc86416bf056f9b15119910f74e5d4c8077
redmictian <redmictian@gmail.com>
2022-01-09 23:59:11 +0300

    arith is finished

--- arith_funcs.sh
+++ arith_funcs.sh
@@ -1,16 +1,33 @@
 add () {
-	echo "adding"
+	echo $(($1+$2))
 }
 subtract () {
-	echo "subtracting"
+	echo $(($1-$2))
 }
 multiply () {
-	echo "Multiplying"
+	echo $(($1*$2))
 }
 divide () {
-	echo "dividing"
+	echo $(($1/$2))
 }
-read -p "Choose what needs to be done: 1 - adding, 2 - subtracting, 3 - multiplying, 4 - dividing" oper
-if [ $oper -eq 1 ]
+read -p "Enter the first number " first
+read -p "Enter the second number " second
+read -p "Choose what needs to be done: a - adding, s - subtracting, m - multiplying, d - dividing " oper
+if [ $oper == "a" ]
 then
-	add()
+	add $first $second
+elif [ $oper == "s" ]
+then
+	subtract $first $second
+elif [ $oper == "m" ]
+then
+	multiply $first $second
+elif [ $oper == "d" ]
+then
+	divide $frist $second
+else
+	echo "You have entered something wrong"
+	exit 0
+fi
+echo "Done"
+exit 1
