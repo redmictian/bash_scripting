8182a3e72d00425f1e25df961b9cab23045a542e
Michael Zhukov <redmictian@gmail.com>
2022-08-07 18:28:03 +0300

    fileupdate v10

--- fileupdate.sh
+++ fileupdate.sh
@@ -3,10 +3,26 @@ echo 'path'
 read path_var
 echo 'file name'
 read filename_var
-echo "The path to the file $(ls -lah $path_var/$filename_var)"
-# cp /var/www/html/about-us/wp-content/themes/blueelephant/header.php header.php-bkp
-# ls -lah
-# sudo chmod --reference=/var/www/html/about-us/wp-content/themes/blueelephant/header.php header.php; sudo chown --reference=/var/www/html/about-us/wp-content/themes/blueelephant/header.php header.php
-# ls -lah
-# sudo mv header.php /var/www/html/about-us/wp-content/themes/blueelephant/
-# ls -lah /var/www/html/about-us/wp-content/themes/blueelephant/header.php
+
+[[ -f $path_var/$filename_var ]] && echo "The path to the old file is $(ls -lah $path_var/$filename_var)" || echo "File to change can't be found $(exit)"
+[[ -f ./$filename_var ]] && echo "The new file is $(ls -lah $filename_var)" || echo "The new file can't be found $(exit)"
+
+read -p "Proceed?" yn
+case $yn in 
+	[yY] ) echo ok, we will proceed;
+		break;;
+	[nN] ) echo exiting...;
+		exit;;
+	* ) echo invalid response;;
+esac
+echo "Proceeding"
+
+echo "Creating a backup"
+backupname_var=$filename_var-$(date +%Y%m%d)-bkp
+cp $path_var/$filename_var $filename_var-$(date +%Y%m%d-%H:%M)-bkp && echo "Backup created" && ls -lah $backupname_var || echo "Backup failed $(exit)"
+
+echo "Changing permissions"
+sudo chmod --reference=$path_var/$filename_var $filename_var && sudo chown --reference=$path_var/$filename_var $filename_var && echo "Done" && ls -lah $filename_var
+
+echo "Replacing an old file with the new"
+sudo mv $filename_var $path_var && ls -lah $path_var/$filename_var && echo "Finished"
